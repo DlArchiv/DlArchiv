605332c83bd77faffbcdedd4c49c347e37db3d9a
Icebair <xell2222@hotmail.com>
2013-11-23 19:58:52 +0100

    Datenbank hinzugefügt
    DB Name : test
    Tabellenname : DLDB
    Primkey : upid
    nick
    titel
    datum

---
+++ DlDb.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 23. Nov 2013 um 19:54
+-- Server Version: 5.5.32
+-- PHP-Version: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `DlDb`
+--
+
+CREATE TABLE IF NOT EXISTS `dldb` (
+  `upid` int(11) NOT NULL AUTO_INCREMENT,
+  `nick` text NOT NULL,
+  `titel` text NOT NULL,
+  `datum` date NOT NULL,
+  PRIMARY KEY (`upid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
